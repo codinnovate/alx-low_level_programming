8cca767b607733651e4bcd477989c02c5d26155e
cody <adeyemis710@gmail.com>
2023-10-13 19:19:59 +0100

    OKS

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,31 +9,25 @@
  */
 
 int main(void)
-
 {
 for (int i = 1; i <= 100; i++)
 {
-
-if (i % 3 == 0)
-
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (i % 3 == 0)
 {
 printf("Fizz ");
 }
-
 else if (i % 5 == 0)
 {
 printf("Buzz ");
-}
-
-else if (i % 3 == 0 && i % 5 == 0)
-{
-printf("FizzBuzz ");
 }
 else
 {
 printf("%d ", i);
 }
-
 }
 printf("\n");
 return (0);
