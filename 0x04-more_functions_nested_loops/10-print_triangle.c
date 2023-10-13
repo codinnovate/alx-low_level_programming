7f4a0a2604aa91b59fca4aef12b9b038c018d02e
cody <adeyemis710@gmail.com>
2023-10-13 19:24:24 +0100

    ok

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,8 +1,25 @@
 #include "main.h"
 
 /**
- * _isdigit - Checks for a digit (0 through 9)
- * @c : is the charater
- * Return: _isdigit returns 1 if c is a digit
- * otherwise 0
+ * print_triangle - prints a right-angled triangle of '#' characters
+ * @size: the size of the triangle
  */
+
+void print_triangle(int size)
+{
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (int row = 1; row <= size; row++)
+{
+for (int col = 1; col <= row; col++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
