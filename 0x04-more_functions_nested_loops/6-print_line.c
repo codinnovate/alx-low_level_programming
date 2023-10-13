bf3a63ec4e7582969564069c9fbca188d2a8253f
cody <adeyemis710@gmail.com>
2023-10-13 18:33:03 +0100

    6-print_line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - prints a straight line of '_' characters
+ * @n: number of times the character '_' should be printed
+ */
+void print_line(int n)
+{
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+while (n > 0)
+{
+_putchar('_');
+n--;
+}
+_putchar('\n');
+}
+}
