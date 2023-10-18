90245ef4daa0669ac5a9d8de6262e2095ab8b059
cody <adeyemis710@gmail.com>
2023-10-18 04:33:02 +0100

    osk

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,7 +11,6 @@ void print_array(int *a, int n)
 {
 int i;
 
-if (n > 0)
 {
 for (i = 0; i < n; i++)
 {
@@ -19,11 +18,10 @@ if (i < n - 1)
 {
 printf("%d, ", a[i]);
 }
-else
+if (i != (n - 1))
 {
-printf("%d", a[i]);
+printf(", ");
 }
 }
 printf("\n");
-}
 }
