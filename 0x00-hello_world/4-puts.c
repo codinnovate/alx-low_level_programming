2caab7f5f7a7428598b76a6576d24129f6c214e6
cody <adeyemis710@gmail.com>
2023-10-18 09:32:20 +0100

    goosd

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-/** 
- * THIS IS FUNCTION 
+/**
+ *main - Entry point
+ * Return: Always 0
  */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
