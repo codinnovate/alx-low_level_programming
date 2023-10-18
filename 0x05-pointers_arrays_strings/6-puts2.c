e54034d5a9913b94e3e48937212c658f46e56ccf
cody <adeyemis710@gmail.com>
2023-10-18 02:40:15 +0100

    print_arrays

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * puts2 - Prints every other character of a string, starting with the first character.
+ * puts2 - Prints every other character of a string,
+ * starting with the first character.
  * @str: A pointer to the string to be printed.
  */
 
--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,9 +8,11 @@
 
 void print_array(int *a, int n)
 {
+int i;
+
 if (n > 0)
 {
-for (int i = 0; i < n; i++)
+for (i = 0; i < n; i++)
 {
 if (i < n - 1)
 {
