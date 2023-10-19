368ae2c00f7858d9f4dbb53c77e0df88bf496d3e
cody <adeyemis710@gmail.com>
2023-10-19 13:32:57 +0100

    print different sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: returns 0
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+printf("Size of char: %zu bytes\n", sizeof(char));
+printf("Size of int: %zu bytes\n", sizeof(int));
+printf("Size of long int: %zu bytes\n", sizeof(long int));
+printf("Size of long long: %zu bytes\n", sizeof(long long));
+printf("Size of float: %zu bytes\n", sizeof(float));
+
+return (0);
+}
